07a280212b018927c9fd32da0afd964938e9d095
ngnam <nguyenvannam0411@gmail.com>
2021-05-31 00:17:57 +0700

    Minimum Tasks - Add a health property to each hero. Default it to 100 - Add a tab for weapons. It has the properties: id, name, damage - Add a tab for armour. It has the properties: id, name, health - Add a weapon property to each hero - Add an armour property to each hero ( a hero’s health is hero.health + armour.health) - Add ImageSrc to each hero. Allows you to add an image - Add a canvas (Konva framework) to the dashboard page

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,20 @@
-export interface Hero {
+export interface hero {
   id: number;
   name: string;
+  imgSrc?: string;
+  health: number;
+  weapon: weapon;
+  armour: armour;
+}
+
+export interface weapon {
+  id: number;
+  name: string;
+  damage: number;
+}
+
+export interface armour {
+  id: number;
+  name: string;
+  health: number;
 }
