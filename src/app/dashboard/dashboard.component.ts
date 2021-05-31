ceb3e7a51757dee6dc27230014064581e6e9e675
Miftahul Arifin <miftahul97@gmail.com>
2021-05-31 10:41:16 +0700

    remove hero when click again

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,6 +15,8 @@ export class DashboardComponent implements OnInit {
   width: number = window.innerWidth*0.6;
   height: number = 500;
   numHero: number = 0;
+  heroLeft: boolean = false;
+  heroRight: boolean = false;
 
   constructor(private heroService: HeroService) { }
 
@@ -34,6 +36,8 @@ export class DashboardComponent implements OnInit {
 
     Konva.Image.fromURL('./assets/background.png', function (bgImage: any) {
       bgImage.setAttrs({
+        id: null,
+        category: 'background',
         x: 0,
         y: 0,
         width: _this.stage.width(),
@@ -51,34 +55,72 @@ export class DashboardComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes.slice(0, 8));
   }
 
+  checkChildrenHero(id: number): boolean {
+    const tr = this.layer.children?.find(item => item.attrs.id == id);
+    if (tr) {
+      return true;
+    }
+
+    return false;
+  }
+
   deployHero(id: number): void {
     const _this = this;
 
-    Konva.Image.fromURL(`./assets/heroes/${id}.png`, function (heroImg: any) {
-      if (_this.numHero == 0) {
-        heroImg.setAttrs({
-          x:100,
-          y: 100,
-          width: 200,
-          height: 400,
-          draggable: false
-        });
-        _this.layer.add(heroImg);
-        _this.numHero++;
-      } else if (_this.numHero == 1) {
-        heroImg.setAttrs({
-          x: _this.width-100,
-          y: 100,
-          width: 200,
-          height: 400,
-          scaleX: -1,
-          draggable: false
-        });
-        _this.layer.add(heroImg);
-        _this.numHero++;
-      }
-    });
+    if (this.checkChildrenHero(id)) {
+      const tr = this.layer.children?.find(item => item.attrs.id == id);
 
-    this.stage.add(this.layer);
+      if (tr?.attrs.scaleX == 1) {
+        this.heroLeft = false;
+      } else if (tr?.attrs.scaleX == -1) {
+        this.heroRight = false;
+      }
+
+      tr?.destroy();
+      this.numHero--;
+      this.layer.draw();
+    } else {
+      Konva.Image.fromURL(`./assets/heroes/${id}.png`, function (heroImg: any) {
+        if (_this.numHero != 2) {
+          if (!_this.heroLeft) {
+            heroImg.setAttrs({
+              id: id,
+              category: 'hero',
+              x: 100,
+              y: 100,
+              width: 200,
+              scaleX: 1,
+              height: 400,
+              draggable: false
+            });
+            _this.heroLeft = true;
+            _this.layer.add(heroImg);
+            _this.numHero++;
+          } else if (!_this.heroRight) {
+            heroImg.setAttrs({
+              id: id,
+              category: 'hero',
+              x: _this.width-100,
+              y: 100,
+              width: 200,
+              height: 400,
+              scaleX: -1,
+              draggable: false
+            });
+            _this.heroRight = true;
+            _this.layer.add(heroImg);
+            _this.numHero++;
+          }
+        }
+      });
+    }
+
+  }
+
+  clearCanvas(): void {
+    const tr = this.layer.children?.find(item => item.attrs.category == 'background');
+    tr?.destroy();
+    this.layer.draw();
+    console.log(this.layer);
   }
 }
